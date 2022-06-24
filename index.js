d12b40b7f93468260d06ee587b97dc84fdc46e86
Lucas Borghi <lucas.borghi@ing.austral.edu.ar>
2022-06-23 23:20:38 -0300

    entrega Eventos

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const cryptos = [];
+const monedas = [];
+const seleccion = [];
+var moneda;
+var maximoMoneda;
+var monto;
+var cryptoElegida;
+var transaccion
+
+
 class Cryptomoneda {
     constructor(nombre, precio){
         this.nombre = nombre
@@ -22,25 +32,36 @@ class Compras {
     }
 }
 
-const cryptos = [];
-const monedas = [];
-const seleccion = [];
+// Declaro Cryptos y divisas disponibles y guardo nombres de divisas y cryptos
 
-cryptos.push(new Cryptomoneda ("Bitcoin", 23500));
-cryptos.push(new Cryptomoneda ("Ethereum", 1250));
+function inicioCryptosDivisas(){
+    cryptos.push(new Cryptomoneda ("Bitcoin", 23500));
+    cryptos.push(new Cryptomoneda ("Ethereum", 1250));
 
-monedas.push(new Divisas ("peso", 121, 36500, 121, "pesos"));
-monedas.push(new Divisas ("dolar", 1, 300, 1, "dolares"));
+    monedas.push(new Divisas ("peso", 121, 36500, 121, "pesos"));
+    monedas.push(new Divisas ("dolar", 1, 300, 1, "dolares"));
 
+    arrayNombres = [];
+    for (const producto of monedas){
+        arrayNombres.push(producto.nombre) 
+    }
 
-arrayNombres = [];
-for (const producto of monedas){
-    arrayNombres.push(producto.nombre) 
+    arrayCryptos = [];
+    for (const producto of cryptos){
+        arrayCryptos.push(producto.nombre)
+    }   
 }
 
-arrayCryptos = [];
-for (const producto of cryptos){
-    arrayCryptos.push(producto.nombre)
+
+function bienvenidaComprar(){
+    alert("Hola! Bienvenido a LucExchange\nEl mejor lugar para obtener las cryptos más famosas");
+    alert("Actualmente tenemos las siguientes criptomonedas");
+
+    for (const lista of cryptos) {
+        alert(lista.nombre + " con precio unitario de " + lista.precio + " dolares.");
+    }
+
+    moneda = prompt ("Ingresá el nombre de moneda con la que realizarás la operación\nPuede ser " + arrayNombres.join(" o "));
 }
 
 //Verifica si se puso dolar o peso
@@ -51,6 +72,12 @@ function verificarMoneda() {
         moneda = prompt ("Intentá nuevamente escribiendo el nombre de tu moneda\nPuede ser " + arrayNombres.join(" o "));
         moneda = moneda.toLowerCase();
     }
+}
+
+//maximo de la moneda y monto a gastar
+function maximoyMonto(){
+    maximoMoneda = monedas.find((el) => el.nombre === moneda)
+    monto = prompt ("Ingresá el monto que quieras invertir\nEl máximo es de "+ maximoMoneda.maximo + " " + maximoMoneda.nombrePlural);
 }
 
 //Verifica que el monto que pone la persona sea peso o dolar sea dentro de los límites
@@ -70,27 +97,37 @@ function dolarizarYDisponible() {
     alert ("Usted tiene " + monto + " dolares para transaccionar");
 }
 
+//PRegunta crypto
+function preguntarCrypto() {
+    cryptoElegida = prompt ("¿Que crypto desea comprar? Elija entre " + arrayCryptos.join(" o "));
+}
+
 //Verifica si elejiste una de las cryptos enlistadas
 function verificarCrypto() {
-    crypto = crypto.toLowerCase();
-    crypto = crypto.charAt(0).toUpperCase() + crypto.slice(1);
+    cryptoElegida = cryptoElegida.toLowerCase();
+    cryptoElegida = cryptoElegida.charAt(0).toUpperCase() + cryptoElegida.slice(1);
     
-    while (cryptos.some(cryptos => cryptos.nombre === crypto) == false){
+    while (cryptos.some(cryptos => cryptos.nombre === cryptoElegida) == false){
 
         alert("El usuario escribió una crypto diferente a las ofrecidas");
-        crypto = prompt ("Intentá nuevamente escribiendo el nombre de su crypto\nPuede ser " + arrayCryptos.join(" o "));
-        crypto = crypto.toLowerCase();
-        crypto = crypto.charAt(0).toUpperCase() + crypto.slice(1);
+        cryptoElegida = prompt ("Intentá nuevamente escribiendo el nombre de su crypto\nPuede ser " + arrayCryptos.join(" o "));
+        cryptoElegida = cryptoElegida.toLowerCase();
+        cryptoElegida = cryptoElegida.charAt(0).toUpperCase() + cryptoElegida.slice(1);
     }  
 }
 
 //Te dice segun tu plata y la crypto seleccionada cuanto vas a obtener
 function compraCrypto() {
-    const buyCrypto = cryptos.find((el) => el.nombre === crypto)
+    const buyCrypto = cryptos.find((el) => el.nombre === cryptoElegida)
     cantidad = monto / buyCrypto.precio
     cantidad = cantidad.toFixed(5)
     alert("La cotización actual del " + buyCrypto.nombre + " es de 1 " + buyCrypto.nombre + " por " + buyCrypto.precio + " dolares. \nUsted puede comprar hasta " + cantidad + " " + buyCrypto.nombre);
 }
+
+function cantidadTransaccionar() {
+    transaccion = prompt("¿Qué cantidad de dólares desea comprar de " + cryptoElegida + "?\nRecordá que tu monto es de " + monto)
+}
+
 
 //verifica cantidad que queres comprar con dinero que se tiene
 function verificarTransaccion() {
@@ -101,6 +138,29 @@ function verificarTransaccion() {
         alert ("Ingresá un número mayor o igual a 1 y dentro de tu monto máximo de " + monto);
         transaccion = prompt ("Ingresá el monto que quieras invertir\nEl máximo es de " + monto); 
     }
+}
+
+function montoFinal() {
+    monto = monto - transaccion;
+    monto = monto.toFixed(2);
+
+    seleccion.push(new Compras (cryptoElegida, transaccion));
+
+    for (const listaCompra of seleccion){
+    alert("Usted realizo la compra de " + listaCompra.nombreCrypto + " por " + listaCompra.precioTrans + " dolares.")}
+
+    alert("Su saldo restante es " + monto);
+
+    if (monto >= 1){
+        seguir = prompt("¿Desea seguir transaccionando? Si o No" );
+
+        verificarSeguirTransaccionando ();
+        seguirTransaccionando ();
+    }
+
+
+    alert("Su transacción fue aprobada. Es la número " + Math.floor((Math.random() * 10000) + 1000));
+    alert("Muchas Gracias por Confiar en Nosotros")
 }
 
 //verifica respuesta del seguir transaccionando
@@ -115,20 +175,20 @@ function verificarSeguirTransaccionando () {
 
 function seguirTransaccionando () {
     while (seguir == "si" && monto >= 1){
-        crypto = prompt ("¿Que crypto desea comprar? Elija entre " + arrayCryptos.join(" o "));
+        cryptoElegida = prompt ("¿Que crypto desea comprar? Elija entre " + arrayCryptos.join(" o "));
 
         verificarCrypto();
 
         compraCrypto();
 
-        transaccion = prompt("¿Qué cantidad de dólares desea comprar de " + crypto + "?\nRecordá que tu monto es de " + monto)
+        transaccion = prompt("¿Qué cantidad de dólares desea comprar de " + cryptoElegida + "?\nRecordá que tu monto es de " + monto)
 
         verificarTransaccion();
 
         monto = monto - transaccion;
         monto = monto.toFixed(2);
 
-        seleccion.push(new Compras (crypto, transaccion));
+        seleccion.push(new Compras (cryptoElegida, transaccion));
 
         alert("Usted realizó las siguientes compras")
 
@@ -145,59 +205,16 @@ function seguirTransaccionando () {
     }
 }
 
-
-
-//Experiencia del cliente
-
-alert("Hola! Bienvenido a LucExchange\nEl mejor lugar para obtener las cryptos más famosas");
-alert("Actualmente tenemos las siguientes criptomonedas");
-
-for (const lista of cryptos) {
-    alert(lista.nombre + " con precio unitario de " + lista.precio + " dolares.");
-}
-
-let moneda = prompt ("Ingresá el nombre de moneda con la que realizarás la operación\nPuede ser " + arrayNombres.join(" o "));
-
+//ejecución de funciones
+inicioCryptosDivisas();
+bienvenidaComprar();
 verificarMoneda();
-
-//Planteo Monto como variable Global
-
-const maximoMoneda = monedas.find((el) => el.nombre === moneda)
-let monto = prompt ("Ingresá el monto que quieras invertir\nEl máximo es de "+ maximoMoneda.maximo + " " + maximoMoneda.nombrePlural);
-
-
+maximoyMonto();
 verificarMonto();
-
 dolarizarYDisponible();
-
-//Planteo Crypto como variable global
-let crypto = prompt ("¿Que crypto desea comprar? Elija entre " + arrayCryptos.join(" o "));
-
+preguntarCrypto()
 verificarCrypto();
-
 compraCrypto();
-
-let transaccion = prompt("¿Qué cantidad de dólares desea comprar de " + crypto + "?\nRecordá que tu monto es de " + monto)
-
+cantidadTransaccionar();
 verificarTransaccion();
-
-monto = monto - transaccion;
-monto = monto.toFixed(2);
-
-seleccion.push(new Compras (crypto, transaccion));
-
-for (const listaCompra of seleccion){
-alert("Usted realizo la compra de " + listaCompra.nombreCrypto + " por " + listaCompra.precioTrans + " dolares.")}
-
-alert("Su saldo restante es " + monto);
-
-if (monto >= 1){
-seguir = prompt("¿Desea seguir transaccionando? Si o No" );
-
-verificarSeguirTransaccionando ();
-seguirTransaccionando ();
-}
-
-
-alert("Su transacción fue aprobada. Es la número " + Math.floor((Math.random() * 10000) + 1000));
-alert("Muchas Gracias por Confiar en Nosotros")
+montoFinal();
---
+++ index2.js
@@ -0,0 +1,222 @@
+const cryptos = [];
+const monedas = [];
+const seleccion = [];
+let createCryptos = document.getElementById("createCryptos")
+let desplegable = document.getElementById("desplegable")
+let desplegable2 = document.getElementById("divisas")
+let montoDivisa = document.getElementById("montoDivisa")
+let montoCrypto = document.getElementById("montoCrypto")
+let mensajeCrypto = document.getElementById("mensajeCrypto")
+let confirmaCompra = document.getElementById("confirmarCompra")
+let mensajeDivisa = document.getElementById("mensajeDivisa")
+let mensajeMoneda = document.getElementById("mensajeMoneda")
+let disclaimer = document.getElementById("disclaimer")
+var moneda;
+var maximoMoneda;
+var monto;
+var cryptoElegida;
+var transaccion;
+var dolarizar;
+var gasto;
+var costoCrypto;
+
+
+class Cryptomoneda {
+    constructor(nombre, precio, grafico){
+        this.nombre = nombre
+        this.precio = precio
+        this.grafico = grafico
+    }
+}
+
+class Divisas  {
+    constructor (nombre, precioDolar, maximo, minimo, nombrePlural){
+        this.nombre = nombre
+        this.precioDolar = precioDolar
+        this.maximo = maximo
+        this.minimo = minimo
+        this.nombrePlural = nombrePlural
+    }
+}
+
+class Compras {
+    constructor(nombreCrypto, precioTrans){
+        this.nombreCrypto = nombreCrypto
+        this.precioTrans = precioTrans
+    }
+}
+
+// Declaro Cryptos y divisas disponibles y guardo nombres de divisas y cryptos
+
+function inicioCryptosDivisas(){
+    cryptos.push(new Cryptomoneda ("Bitcoin", 23500, ` <div style="width: 250px; height:220px; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; block-size:220px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #56667F;padding:1px;padding: 0px; margin: 0px;"><div style="height:200px; padding:0px; margin:0px; width: 100%;"><iframe src="https://widget.coinlib.io/widget?type=single_v2&theme=light&coin_id=859&pref_coin_id=1505" width="250" height="196px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;line-height:14px;"></iframe></div><div style="color: #FFFFFF; line-height: 14px; font-weight: 400; font-size: 11px; box-sizing: border-box; padding: 2px 6px; width: 100%; font-family: Verdana, Tahoma, Arial, sans-serif;"><a href="https://coinlib.io" target="_blank" style="font-weight: 500; color: #FFFFFF; text-decoration:none; font-size:11px">Cryptocurrency Prices</a>&nbsp;by Coinlib</div></div>` ));
+    cryptos.push(new Cryptomoneda ("Ethereum", 1250, ` <div style="width: 250px; height:220px; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; block-size:220px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #56667F;padding:1px;padding: 0px; margin: 0px;"><div style="height:200px; padding:0px; margin:0px; width: 100%;"><iframe src="https://widget.coinlib.io/widget?type=single_v2&theme=light&coin_id=145&pref_coin_id=1505" width="250" height="196px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;line-height:14px;"></iframe></div><div style="color: #FFFFFF; line-height: 14px; font-weight: 400; font-size: 11px; box-sizing: border-box; padding: 2px 6px; width: 100%; font-family: Verdana, Tahoma, Arial, sans-serif;"><a href="https://coinlib.io" target="_blank" style="font-weight: 500; color: #FFFFFF; text-decoration:none; font-size:11px">Cryptocurrency Prices</a>&nbsp;by Coinlib</div></div>` ));
+
+    monedas.push(new Divisas ("Peso", 121, 36500, 121, "pesos"));
+    monedas.push(new Divisas ("Dólar", 1, 300, 1, "dolares"));
+
+    arrayNombres = [];
+    for (const producto of monedas){
+        arrayNombres.push(producto.nombre) 
+    }
+
+    arrayCryptos = [];
+    for (const producto of cryptos){
+        arrayCryptos.push(producto.nombre)
+    }   
+}
+
+function mostrarCryptos() {
+    cryptos.forEach((crypto) =>{
+    let card = document.createElement('div')
+    card.innerHTML += ` <div class="card" style="width: 18rem;">
+    <h5 class="card-title text-center">${crypto.nombre}</h5>
+    <div class= "container-fluid"> ${crypto.grafico} </div>
+    <div class="card-body d-flex justify-content-center">
+    </div> </div>` 
+    createCryptos.append(card)
+})
+}
+
+function desplegableCryptos() {
+    cryptos.forEach((crypto) =>{
+        let desple = document.createElement('option')
+        desple.innerHTML += ` ${crypto.nombre} ` 
+        desplegable.append(desple)
+    })
+}
+
+function desplegableCryptos() {
+    cryptos.forEach((crypto) =>{
+        let desple = document.createElement('option')
+        desple.innerHTML += ` ${crypto.nombre} ` 
+        desplegable.append(desple)
+    })
+    desplegable.onchange = () => console.log(desplegable.value)
+}
+
+function desplegableDivisas() {
+    monedas.forEach((moneda) =>{
+        let desple2 = document.createElement('option')
+        desple2.innerHTML += ` ${moneda.nombre} ` 
+        desplegable2.append(desple2)
+    })
+    desplegable2.onchange = () => {console.log(desplegable2.value)
+    const buyMonto = monedas.find((el) => el.nombre === desplegable2.value)
+    let mensajitoMoneda = document.createElement('div')
+    if (buyMonto === undefined){
+        let mensajitoMoneda = document.createElement('div')
+        mensajeMoneda.innerText = " "
+        mensajitoMoneda.innerHTML += `<p class="text-danger"> Selecciona una moneda primero </p> ` 
+        mensajeMoneda.append(mensajitoMoneda)
+    }
+    else{
+    mensajeMoneda.innerText = " "
+    mensajeDivisa.innerText = " "
+    montoDivisa.value = " ";
+    let mensajitoDivisa = document.createElement('div')
+    mensajeDivisa.innerText = " "
+    mensajitoDivisa.innerHTML += `<p class="text-success"> Elegiste ${buyMonto.nombre} recuerda que debe ser un número dentro del límite especificado entre  ${buyMonto.minimo} y ${buyMonto.maximo} ${buyMonto.nombrePlural} </p> ` 
+    mensajeDivisa.append(mensajitoDivisa)
+    }
+    }
+}
+
+function definirMonto() {
+    montoDivisa.onchange = () => { console.log(montoDivisa.value)
+    const buyMonto = monedas.find((el) => el.nombre === desplegable2.value)
+    console.log(buyMonto)
+    let mensajitoDivisa = document.createElement('div')
+    if (buyMonto === undefined){
+        mensajeDivisa.innerText = " "
+        mensajitoDivisa.innerHTML += `<p class="text-danger"> Primero selecciona una moneda </p> ` 
+        mensajeDivisa.append(mensajitoDivisa)
+        monto = "nook"
+    }
+
+    if (buyMonto !== undefined){
+    if ((montoDivisa.value === ''|| isNaN(montoDivisa.value) && (buyMonto.nombre == desplegable2.value))|| ((buyMonto.nombre == desplegable2.value) && (montoDivisa.value < buyMonto.minimo))|| ((buyMonto.nombre == desplegable2.value) && (montoDivisa.value > buyMonto.maximo))){
+        mensajeDivisa.innerText = " "
+        mensajitoDivisa.innerHTML += `<p class="text-danger"> No es un monto válido, recuerda que debe serun número dentro del límite especificado entre  ${buyMonto.minimo} y ${buyMonto.maximo} ${buyMonto.nombrePlural} </p> ` 
+        mensajeDivisa.append(mensajitoDivisa)
+        monto = "nook"
+    }
+    else{
+        mensajeDivisa.innerText = " "
+        monto = "ok"
+    }
+}}}
+
+function definirCrypto() {
+    montoCrypto.onchange = () => { console.log(montoCrypto.value)
+    const buyMonto = monedas.find((el) => el.nombre === desplegable2.value)
+    const buyCrypto = cryptos.find((el) => el.nombre === desplegable.value)
+    let mensajitoCrypto = document.createElement('div')
+    let dolarizar = montoDivisa.value/buyMonto.precioDolar;
+    let gasto = dolarizar/buyCrypto.precio;
+    dolarizar = dolarizar.toFixed(2)
+    gasto = gasto.toFixed(8)
+    if (buyMonto == undefined || buyCrypto == undefined || monto != "ok" ){
+        mensajeCrypto.innerText = " "
+        mensajitoCrypto.innerHTML += `<p class="text-danger"> Primero debes seleccionar una Crypto, una moneda y monto </p> ` 
+        mensajeCrypto.append(mensajitoCrypto)
+        transaccion = "nook"
+    }
+    if (buyMonto !== undefined && montoCrypto.value > gasto){
+        mensajeCrypto.innerText = " "
+        mensajitoCrypto.innerHTML += `<p class="text-danger"> Te pasaste del límite </p> ` 
+        mensajeCrypto.append(mensajitoCrypto)
+        transaccion = "nook"
+    }
+    if (buyMonto !== undefined && montoCrypto.value <= gasto){
+        mensajeCrypto.innerText = " "
+        mensajitoCrypto.innerHTML += `<p class="text-success"> Confirma tu compra </p> ` 
+        mensajeCrypto.append(mensajitoCrypto) 
+        transaccion = "ok"
+        let botonCompra = document.createElement("div")
+        botonCompra.innerHTML += `<button type="button" onclick="alertaConfirmacion()" class="btn btn-primary"> Confirmar Compra </button> `
+        confirmaCompra.append(botonCompra) 
+    }
+    }
+}
+
+function seleccionadorCrypto(){
+    desplegable.onchange = () => {
+    const buyMonto = monedas.find((el) => el.nombre === desplegable2.value)
+    const buyCrypto = cryptos.find((el) => el.nombre === desplegable.value)
+    let dolarizar = montoDivisa.value/buyMonto.precioDolar;
+    let gasto = dolarizar/buyCrypto.precio;
+    dolarizar = dolarizar.toFixed(2)
+    gasto = gasto.toFixed(8)
+        if (buyMonto.nombre !== undefined && buyCrypto.nombre !== undefined){
+            let alerta = document.createElement("div")
+            disclaimer.innerText = " "
+            alerta.innerHTML += `<p class="text-primary"> Tienes ${dolarizar} dólares para comprar hasta ${gasto} de ${buyCrypto.nombre} </p> ` 
+            disclaimer.append(alerta)
+        }
+    }
+}
+
+
+function alertaConfirmacion(){
+    const buyCrypto = cryptos.find((el) => el.nombre === desplegable.value)
+    const buyMonto = monedas.find((el) => el.nombre === desplegable2.value)
+    costoCrypto = montoCrypto.value*buyCrypto.precio
+    let dolarizar = montoDivisa.value/buyMonto.precioDolar;
+    dolarizar = dolarizar.toFixed(2)
+    console.log(costoCrypto)
+    console.log(dolarizar)
+    dolarizar = dolarizar-costoCrypto
+    console.log(dolarizar)
+    alert("Tu compra fue satisfactoria! Compraste " + montoCrypto.value + " de " + buyCrypto.nombre + " y tu saldo restante es de " + dolarizar + " dólares.")
+}
+
+
+
+inicioCryptosDivisas();
+mostrarCryptos();
+desplegableCryptos();
+desplegableDivisas();
+definirMonto();
+definirCrypto();
+seleccionadorCrypto();
